70c61e431310c18362a765f06bfc0b38d00472a8
Denys Burduzhan <151670272+DenysBurduzhan@users.noreply.github.com>
2026-09-11 04:02:10 +0300

    removed comments

--- Traffic_Light/Traffic_Light(ESP-IDF)/main/GPIO_driver/GPIO.cpp
+++ Traffic_Light/Traffic_Light(ESP-IDF)/main/GPIO_driver/GPIO.cpp
@@ -19,19 +19,16 @@ bool GPIO::initGPIO(uint8_t pin){
     return true;
 }
 void GPIO::enableOutput(uint8_t pin) {
-    //REG_SET_BIT(GPIO_ENABLE_REG, (1UL << pin));
     volatile uint32_t* gpio_enable_reg = (volatile uint32_t*)GPIO_ENABLE_REG;
     *gpio_enable_reg |= (1UL << pin);
 }
 
 void GPIO::setOutput(uint8_t pin) {
-    //REG_WRITE(GPIO_OUT_W1TS_REG, (1UL << pin));
     volatile uint32_t* gpio_out_w1ts_reg = (volatile uint32_t*)GPIO_OUT_W1TS_REG;
     *gpio_out_w1ts_reg = (1UL << pin);
 }
 
 void GPIO::clearOutput(uint8_t pin) {
-    //REG_WRITE(GPIO_OUT_W1TC_REG, (1UL << pin));
     volatile uint32_t* gpio_out_w1tc_reg = (volatile uint32_t*)GPIO_OUT_W1TC_REG;
     *gpio_out_w1tc_reg = (1UL << pin);
 }
