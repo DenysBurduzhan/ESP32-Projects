6708711fad3c72f2118e89e852a5682c04b04b9f
Denys Burduzhan <151670272+DenysBurduzhan@users.noreply.github.com>
2026-09-14 20:26:54 +0300

    extract to UART_Init()

--- UART_Terminal/UART_Terminal/main/main.cpp
+++ UART_Terminal/UART_Terminal/main/main.cpp
@@ -30,8 +30,7 @@ std::string readFromUART(uint8_t rx_buffer[BUF_SIZE], int &rx_index) {
         return receivedString;
 }
 
-
-extern "C" void app_main(void) {
+void UART_Init() {
     uart_config_t uart_config = {};
     uart_config.baud_rate = 115200;
     uart_config.data_bits = UART_DATA_8_BITS;
@@ -43,6 +42,11 @@ extern "C" void app_main(void) {
     ESP_ERROR_CHECK(uart_param_config(UART_NUM_0, &uart_config));
     ESP_ERROR_CHECK(uart_set_pin(UART_NUM_0, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE));
     ESP_ERROR_CHECK(uart_driver_install(UART_NUM_0, BUF_SIZE * 2, 0, 0, NULL, 0));
+}
+
+
+extern "C" void app_main(void) {
+    UART_Init();
 
     uint8_t rx_buffer[BUF_SIZE];
     int rx_index = 0;
