7fb1ee0f80e8ccea9e6694654ec97777dda220d3
Denys Burduzhan <151670272+DenysBurduzhan@users.noreply.github.com>
2026-09-14 18:15:14 +0300

    init pins for terminal

--- UART_Terminal/UART_Terminal/main/main.cpp
+++ UART_Terminal/UART_Terminal/main/main.cpp
@@ -1,4 +1,16 @@
 #include <cstdio>
+#include <string>
+#include "GPIO_driver/GPIO.hpp"
+
+#define ledPIN  25
+#define DHT11_PIN  26
+#define DHTTYPE  DHT11
+#define servoPIN  27
+
+
+uint32_t startTime;
+uint32_t interval = 1000;
+
 
 extern "C" void app_main(void)
 {
